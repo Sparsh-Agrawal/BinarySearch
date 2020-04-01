2667e7445bb3c0d89cc78cc107a823107f6d7897
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-04-01 12:38:47 +0530

    commit

--- src/problem1/recursive/BinarySearch.java
+++ src/problem1/recursive/BinarySearch.java
@@ -35,7 +35,7 @@ public class BinarySearch
     public static void main(String[] args)
     {
         int[] arr = {10,20,30,40,50,60,70};
-        int e = 100;
+        int e = 70;
         BinarySearch o = new BinarySearch(arr);
         int l = 0;
         int h = arr.length - 1;
